5006a9ec24281e6e4cb035050aad26e79a212844
mohammed albait <mohammed albait>
2016-07-01 01:01:56 -0600

    initial commit

---
+++ app/src/main/java/com/example/mohammed/currencyconverter/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.mohammed.currencyconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void convert(View view){
+
+        EditText dollarField =(EditText) findViewById(R.id.dollarField);
+
+        double dollarAmount= Double.parseDouble(dollarField.getText().toString());
+
+        Double poundAmount= dollarAmount*0.65;
+
+        //Log.i("poundAmount", poundAmount.toString());
+        // toast is used to show  the result to the user
+        Toast.makeText(getApplicationContext(),"£"+poundAmount.toString(),Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
